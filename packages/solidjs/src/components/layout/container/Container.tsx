a279e972bb533f6a7f0c04c3f531bfed1519e1d7
cuikho210 <tonykms210@gmail.com>
2024-06-19 10:50:03 +0700

    update container component

--- packages/solidjs/src/components/layout/container/Container.tsx
+++ packages/solidjs/src/components/layout/container/Container.tsx
@@ -5,10 +5,6 @@ import '@vuuui/styles/components/layout/container.scss'
 export const Container: ContainerComponent = (props) => {
 	const className = createMemo(() => {
 		let className = 'vuuui-container '
-
-		if (props.xs) className += 'vuuui-container-xs'
-		if (props.sm) className += 'vuuui-container-sm'
-		if (props.md) className += 'vuuui-container-md'
 		if (props.class) className += props.class
 		return className
 	})
