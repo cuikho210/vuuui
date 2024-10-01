2e51d6c538a3bdc1b43de0623f0780a2c48cd213
cuikho210 <tonykms210@gmail.com>
2024-10-01 21:19:02 +0700

    optimize solidjs/src/components/inputs/Slider.tsx

--- packages/solidjs/src/components/inputs/Slider.tsx
+++ packages/solidjs/src/components/inputs/Slider.tsx
@@ -1,4 +1,9 @@
-import { createMemo, createSignal, createRenderEffect } from 'solid-js'
+import {
+  createMemo,
+  createSignal,
+  createRenderEffect,
+  onCleanup,
+} from 'solid-js'
 import type { SliderComponent } from './types'
 import '@vuuui/styles/components/inputs/slider.scss'
 
@@ -10,30 +15,22 @@ export const Slider: SliderComponent = (props) => {
   let sliderBarRef: HTMLDivElement | undefined
   let beforeClientX: number | null = null
 
-  const className = createMemo<string>(() => {
-    return 'vuuui-slider-wrapper ' + (props.class || '')
+  const className = createMemo<string>(
+    () => 'vuuui-slider-wrapper ' + (props.class || ''),
+  )
+
+  const getRange = () => ({
+    min: Number(props.min) || 0,
+    max: Number(props.max) || 100,
+    range: (Number(props.max) || 100) - (Number(props.min) || 0),
   })
 
-  const getRange = () => {
-    const min = Number(props.min) || 0
-    const max = Number(props.max) || 100
-    const range = max - min
-
-    return { min, max, range }
-  }
-
-  const validatePercent = (value: number) => {
-    if (value > 100) value = 100
-    if (value < 0) value = 0
-    return value
-  }
+  const validatePercent = (value: number) => Math.max(0, Math.min(100, value))
 
   const setModelValue = (percentValue: number) => {
     if (!setModel) return
-
     const { min, range } = getRange()
-    const modelValue = Math.round((percentValue / 100) * range + min)
-    setModel(modelValue)
+    setModel(Math.round((percentValue / 100) * range + min))
   }
 
   const onSliderRef = (sliderBar: HTMLDivElement) => {
@@ -42,14 +39,7 @@ export const Slider: SliderComponent = (props) => {
 
   const onKnobBodyRef = (knob: HTMLDivElement) => {
     const onDragging = (clientX: number) => {
-      if (!knobActive())
-        return console.log(
-          '[knob dragging] rejected by isKnobActive equal false',
-        )
-      if (!sliderBarRef)
-        return console.log(
-          '[knob dragging] rejected by sliderBarRef is undefined',
-        )
+      if (!knobActive() || !sliderBarRef) return
 
       if (beforeClientX == null) {
         beforeClientX = clientX
@@ -67,43 +57,46 @@ export const Slider: SliderComponent = (props) => {
       setModelValue(newPercent)
     }
 
-    const onDragStart = () => {
-      setKnobActive(true)
-    }
-
+    const onDragStart = () => setKnobActive(true)
     const onDragEnd = () => {
       setKnobActive(false)
       beforeClientX = null
     }
 
-    knob.onmousedown = () => onDragStart()
-    knob.ontouchstart = () => onDragStart()
+    knob.onmousedown = onDragStart
+    knob.ontouchstart = onDragStart
 
-    window.addEventListener('mouseup', () => onDragEnd())
-    window.addEventListener('touchend', () => onDragEnd())
+    const mouseupHandler = () => onDragEnd()
+    const mousemoveHandler = (event: MouseEvent) => onDragging(event.clientX)
+    const touchmoveHandler = (event: TouchEvent) =>
+      onDragging(event.touches[0].clientX)
 
-    window.addEventListener('mousemove', (event) => onDragging(event.clientX))
-    window.addEventListener('touchmove', (event) =>
-      onDragging(event.touches[0].clientX),
-    )
+    window.addEventListener('mouseup', mouseupHandler)
+    window.addEventListener('touchend', mouseupHandler)
+    window.addEventListener('mousemove', mousemoveHandler)
+    window.addEventListener('touchmove', touchmoveHandler)
+
+    onCleanup(() => {
+      window.removeEventListener('mouseup', mouseupHandler)
+      window.removeEventListener('touchend', mouseupHandler)
+      window.removeEventListener('mousemove', mousemoveHandler)
+      window.removeEventListener('touchmove', touchmoveHandler)
+    })
   }
 
   createRenderEffect(() => {
-    if (!model) return
-    if (knobActive()) return
-
+    if (!model || knobActive()) return
     const { min, range } = getRange()
-    const percentValue = validatePercent(((model() - min) / range) * 100)
-    setPercent(percentValue)
+    setPercent(validatePercent(((model() - min) / range) * 100))
   })
 
   return (
     <div {...props} class={className()}>
       <div class="vuuui-slider" ref={onSliderRef}>
-        <div class="vuuui-slider-filler" style={{ width: percent() + '%' }} />
-        <div class="vuuui-slider-knob" style={{ left: percent() + '%' }}>
+        <div class="vuuui-slider-filler" style={{ width: `${percent()}%` }} />
+        <div class="vuuui-slider-knob" style={{ left: `${percent()}%` }}>
           <div
-            class={'vuuui-knob-body' + (knobActive() ? ' vuuui-active' : '')}
+            class={`vuuui-knob-body${knobActive() ? ' vuuui-active' : ''}`}
             ref={onKnobBodyRef}
           >
             <div class="vuuui-knob-main" />
