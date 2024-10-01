ed5a18f1fe29a0ec4e759b58f9894cc7ee8be39c
cuikho210 <tonykms210@gmail.com>
2024-10-01 22:23:15 +0700

    vue | create Dialog component

--- packages/vue/preview/App.vue
+++ packages/vue/preview/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { RiHeartLine, RiFileLine, RiPaletteLine } from '@remixicon/vue'
+import {
+  RiHeartLine,
+  RiFileLine,
+  RiPaletteLine,
+  RiCloseLine,
+} from '@remixicon/vue'
 import {
   Container,
   Spacer,
@@ -11,12 +16,15 @@ import {
   Radio,
   Slider,
   Switch,
+  Dialog,
+  IconButton,
 } from '../src'
 
 const file = ref<File | undefined>(undefined)
 const checked = ref(true)
 const switched = ref(true)
 const sliderValue = ref(11)
+const isDialogOpen = ref(false)
 </script>
 
 <template>
@@ -25,6 +33,7 @@ const sliderValue = ref(11)
       <Checkbox placeholder="display file input" v-model="checked" />
 
       <Switch v-model="switched" placeholder="Display color input" />
+      <Switch v-model="isDialogOpen" placeholder="Open dialog" />
 
       <Radio name="radio" placeholder="ahihi radio 1" />
       <Radio name="radio" placeholder="ahihi radio 2" />
@@ -49,6 +58,33 @@ const sliderValue = ref(11)
       <br />
 
       <Slider :min="8" :max="32" v-model="sliderValue" />
+    </div>
+    <br />
+
+    <div>
+      <Dialog v-model="isDialogOpen" backdrop-close>
+        <template #btnAction>
+          <IconButton>
+            <RiHeartLine />
+          </IconButton>
+        </template>
+
+        <template #btnClose>
+          <IconButton @click="isDialogOpen = false">
+            <RiCloseLine />
+          </IconButton>
+        </template>
+
+        <template #title>Ahihi do ngoc~~!</template>
+
+        <template #default>
+          <p>Roses are red,</p>
+          <p>Violets are blue,</p>
+          <p>This dialog is neat,</p>
+          <p>And so are you!</p>
+          <p style="text-align: right"><i>Author: Claude sonnet from zed</i></p>
+        </template>
+      </Dialog>
     </div>
     <br />
   </Container>
---
+++ packages/vue/src/components/overlay/Dialog.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { Container } from '../../'
+import '@vuuui/styles/components/overlay/dialog.scss'
+
+interface DialogProps {
+  modelValue?: boolean
+  backdropClose?: boolean
+}
+
+const props = defineProps<DialogProps>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: boolean): void
+}>()
+
+const backdropClose = () => {
+  if (!props.backdropClose) return
+  emit('update:modelValue', false)
+}
+</script>
+
+<template>
+  <dialog :open="modelValue" title="" class="vuuui-dialog" tabindex="-1">
+    <div class="vuuui-backdrop" @click="backdropClose"></div>
+    <Container class="vuuui-body">
+      <div class="vuuui-title">
+        <div class="vuuui-title-left">
+          <div class="vuuui-button-slot">
+            <slot name="btnAction" />
+          </div>
+
+          <span class="vuuui-title-content">
+            <slot name="title" />
+          </span>
+        </div>
+
+        <div class="vuuui-button-slot">
+          <slot name="btnClose" />
+        </div>
+      </div>
+
+      <div class="vuuui-content">
+        <slot></slot>
+      </div>
+    </Container>
+  </dialog>
+</template>
