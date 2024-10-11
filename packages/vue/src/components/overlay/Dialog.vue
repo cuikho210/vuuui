a7a0a2587182d8183b7cdc30024cd79548743590
cuikho210 <tonykms210@gmail.com>
2024-10-12 00:13:21 +0700

    using v-if and vue transition for dialog body

--- packages/vue/src/components/overlay/Dialog.vue
+++ packages/vue/src/components/overlay/Dialog.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Container } from '../../'
 import '@vuuui/styles/components/overlay/dialog.scss'
+import '@vuuui/styles/transition.scss'
 
 interface DialogProps {
   modelValue?: boolean
@@ -22,26 +23,29 @@ const backdropClose = () => {
 <template>
   <dialog :open="modelValue" title="" class="vuuui-dialog" tabindex="-1">
     <div class="vuuui-backdrop" @click="backdropClose"></div>
-    <Container class="vuuui-body">
-      <div class="vuuui-title">
-        <div class="vuuui-title-left">
-          <div class="vuuui-button-slot">
-            <slot name="btnAction" />
+
+    <Transition name="vuuui-fade-up">
+      <Container class="vuuui-body" v-if="modelValue">
+        <div class="vuuui-title">
+          <div class="vuuui-title-left">
+            <div class="vuuui-button-slot">
+              <slot name="btnAction" />
+            </div>
+
+            <span class="vuuui-title-content">
+              <slot name="title" />
+            </span>
           </div>
 
-          <span class="vuuui-title-content">
-            <slot name="title" />
-          </span>
+          <div class="vuuui-button-slot">
+            <slot name="btnClose" />
+          </div>
         </div>
 
-        <div class="vuuui-button-slot">
-          <slot name="btnClose" />
+        <div class="vuuui-content">
+          <slot></slot>
         </div>
-      </div>
-
-      <div class="vuuui-content">
-        <slot></slot>
-      </div>
-    </Container>
+      </Container>
+    </Transition>
   </dialog>
 </template>
