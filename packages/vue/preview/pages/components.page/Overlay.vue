eef913e812f7d96311c638f430596d7775345b16
cuikho210 <tonykms210@gmail.com>
2024-10-12 00:00:57 +0700

    styles | fix error on nested dialog

--- packages/vue/preview/pages/components.page/Overlay.vue
+++ packages/vue/preview/pages/components.page/Overlay.vue
@@ -1,23 +1,10 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { RiEmotionHappyLine, RiCloseLine } from '@remixicon/vue'
-import {
-  Spacer,
-  ElevatedButton,
-  Dialog,
-  TextButton,
-  IconButton,
-} from '../../../src'
+import { Spacer, ElevatedButton, Dialog, IconButton } from '../../../src'
 
-const dialogOpen = ref(false)
-
-const openDialog = () => {
-  dialogOpen.value = true
-}
-
-const closeDialog = () => {
-  dialogOpen.value = false
-}
+const dialog1Open = ref(false)
+const dialog2Open = ref(false)
 </script>
 
 <template>
@@ -26,18 +13,18 @@ const closeDialog = () => {
     <br />
 
     <Spacer>
-      <ElevatedButton @click="openDialog">
+      <ElevatedButton @click="dialog1Open = true">
         <template #icon>
           <RiEmotionHappyLine />
         </template>
         Open dialog
       </ElevatedButton>
 
-      <Dialog v-model="dialogOpen" backdrop-close>
+      <Dialog v-model="dialog1Open" backdrop-close>
         <template #btnAction> </template>
 
         <template #btnClose>
-          <IconButton @click="closeDialog">
+          <IconButton @click="dialog1Open = false">
             <RiCloseLine />
           </IconButton>
         </template>
@@ -46,7 +33,41 @@ const closeDialog = () => {
           This is a very very very very very very very very loooong title
         </template>
 
-        <template #default> Test dialog ahihi </template>
+        <template #default>
+          <p>This is the content ahihi</p>
+          <br />
+
+          <ElevatedButton @click="dialog2Open = true">
+            Open dialog 2
+          </ElevatedButton>
+
+          <Dialog v-model="dialog2Open" backdrop-close>
+            <template #btnAction> </template>
+
+            <template #btnClose>
+              <IconButton @click="dialog2Open = false">
+                <RiCloseLine />
+              </IconButton>
+            </template>
+
+            <template #title>
+              This is a very very very very very very very very loooong title
+            </template>
+
+            <template #default>
+              <p><i> Trẻ con sa mạc truyền tai nhau bài đồng dao: </i></p>
+              <p>Xương rồng đơm lá đơm hoa</p>
+              <p>Nước đong đầy trên cao nguyên đá</p>
+              <p>Là ngày hoàng đế trở về nhà</p>
+              <br />
+
+              <p><i>Desert children pass on a nursery rhyme:</i></p>
+              <p>Cactus sprouts leaves and flowers</p>
+              <p>Water fills up on the rocky plateau</p>
+              <p>It's the day the emperor returns home</p>
+            </template>
+          </Dialog>
+        </template>
       </Dialog>
     </Spacer>
   </section>
