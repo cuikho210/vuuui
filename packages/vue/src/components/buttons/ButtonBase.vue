c8f6fbcabf484cea70bbcd3cea00b7ea3720555f
cuikho210 <tonykms210@gmail.com>
2024-10-17 14:42:15 +0700

    vue | update ButtonBase.vue

--- packages/vue/src/components/buttons/ButtonBase.vue
+++ packages/vue/src/components/buttons/ButtonBase.vue
@@ -3,26 +3,13 @@ import { computed, defineProps } from 'vue'
 import type { ButtonBaseProps } from './types.d.ts'
 
 const props = defineProps<ButtonBaseProps>()
-
-const emit = defineEmits<{
-  (e: 'click'): void
-}>()
-
 const isDisabled = computed(() => props.loading || props.disabled)
-
-const handleClick = () => {
-  if (!isDisabled.value) {
-    emit('click')
-  }
-}
 </script>
 
 <template>
   <button
-    v-bind="$attrs"
     :disabled="isDisabled"
     :active="active"
-    @click="handleClick"
   >
     <slot />
   </button>
