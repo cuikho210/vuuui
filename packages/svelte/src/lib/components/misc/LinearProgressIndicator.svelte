fc0fe76c02462d06e01621629b7066eb1a97dd15
cuikho210 <tonykms210@gmail.com>
2025-03-03 16:43:57 +0700

    feat(svelte): add misc components for spinners and gap
    - Add DuckSpinner, LinearProgressIndicator, and Gap components
    - Import and export components in index.ts for reusability
    - Include peer dependency for @vuuui/styles

---
+++ packages/svelte/src/lib/components/misc/LinearProgressIndicator.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import '@vuuui/styles/components/misc/spinner/linear_progress_indicator.scss'
+</script>
+
+<div class="vuuui-linear-progress-indicator">
+	<div class="vuuui-linear-progress-indicator-line"></div>
+</div>
---
+++ packages/svelte/src/lib/components/misc/index.ts
@@ -0,0 +1,5 @@
+import DuckSpinner from './DuckSpinner.svelte'
+import LinearProgressIndicator from './LinearProgressIndicator.svelte'
+import Gap from './Gap.svelte'
+
+export { DuckSpinner, LinearProgressIndicator, Gap }
